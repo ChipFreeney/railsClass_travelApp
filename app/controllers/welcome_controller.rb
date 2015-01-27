d09a07d486c1b55705a0c142e0e2d1cca2f0cfc8
Chip Freeney <cfreeney@lat-cfreeney1.cb.careerbuilder.com careerbuilder.com>
2015-01-26 20:23:39 -0500

    pictures

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,21 @@
 class WelcomeController < ApplicationController
   def index
+    @homeland = 'Russia'
+    @countries = ["US", "Canada", "Mexico"]
+    @family = {
+      "Dad" => "Fred", 
+      "Mom" => "Danna", 
+      "Brother" => "Brad", 
+      "Sister" => "Alex", 
+    }
   end
 
   def about
+    @images = {
+      "vapor_trail_chip" => "jpg",
+      "AR_chip" => "png", 
+      "glock_chip" => "png", 
+      "flame_will" => "png"
+    }
   end
 end
