47456e3e78383f5558480b32d091d16670e32619
Chip Freeney <cfreeney@lat-cfreeney1.cb.careerbuilder.com careerbuilder.com>
2015-01-28 20:34:43 -0500

    added session stuff

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,39 @@
 class WelcomeController < ApplicationController
+  before_action :set_color
+
+  # action
   def index
 
     @welcome = params[:welcome]
     @referral = params[:referral]
     @nightmode = params[:nightmode]
-    @background = params[:background]
 
   end
 
+  # action
   def about
+
     @referral = params[:referral]
+
   end
+
+
+  private 
+  def set_color
+
+    if params[:background]
+      session[:background] = params[:background]
+    end
+    @background = session[:background]
+
+  end
+
 end
+
+
+
+
+
+
+
+
